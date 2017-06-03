48fe96136948c1437291ae018dabbfd3864d617c
Mohammad Mirrajabi <mohammadmirrajabi@gmail.com>
2017-06-03 20:46:34 +0430

    Update sample app Closes issue #1

--- app/src/main/java/ir/mirrajabi/searchdialog/sample/MainActivity.java
+++ app/src/main/java/ir/mirrajabi/searchdialog/sample/MainActivity.java
@@ -2,57 +2,129 @@ package ir.mirrajabi.searchdialog.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import ir.mirrajabi.okhttpjsonmock.interceptors.OkHttpMockInterceptor;
 import ir.mirrajabi.searchdialog.SimpleSearchDialogCompat;
+import ir.mirrajabi.searchdialog.core.BaseFilter;
 import ir.mirrajabi.searchdialog.core.BaseSearchDialogCompat;
 import ir.mirrajabi.searchdialog.core.SearchResultListener;
+import ir.mirrajabi.searchdialog.core.Searchable;
 import ir.mirrajabi.searchdialog.sample.customdialogs.ContactSearchDialogCompat;
 import ir.mirrajabi.searchdialog.sample.customdialogs.models.ContactModel;
+import ir.mirrajabi.searchdialog.sample.customdialogs.models.UserModel;
+import ir.mirrajabi.searchdialog.sample.customdialogs.services.UsersService;
 import ir.mirrajabi.searchdialog.sample.models.SampleModel;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Retrofit mRetrofit;
+    private OkHttpClient mOkHttpClient;
+    private UsersService mUsersService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mOkHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(new OkHttpMockInterceptor(this, 5))
+                .build();
+        mRetrofit = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("http://example.com")
+                .client(mOkHttpClient)
+                .build();
+        mUsersService = mRetrofit.create(UsersService.class);
+    }
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    @OnClick(R.id.button)
+    void provideSimpleDialog(){
+        new SimpleSearchDialogCompat(MainActivity.this, "Search...",
+                "What are you looking for...?", null, createSampleData(),
+                new SearchResultListener<SampleModel>() {
+                    @Override
+                    public void onSelected(BaseSearchDialogCompat dialog,
+                                           SampleModel item, int position) {
+                        Toast.makeText(MainActivity.this, item.getTitle(),
+                                Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                }).show();
+    }
+
+    @OnClick(R.id.button2)
+    void provide2CustomContactDialog(){
+        new ContactSearchDialogCompat<>(MainActivity.this, "Search...",
+                "What are you looking for...?", null, createSampleContacts(),
+                new SearchResultListener<ContactModel>() {
+                    @Override
+                    public void onSelected(BaseSearchDialogCompat dialog,
+                                           ContactModel item, int position) {
+                        Toast.makeText(MainActivity.this, item.getTitle(),
+                                Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                }).show();
+    }
+
+    @OnClick(R.id.button3)
+    void provideSimpleDialogWithApiCalls() {
+        final SimpleSearchDialogCompat<UserModel> searchDialog =
                 new SimpleSearchDialogCompat(MainActivity.this, "Search...",
-                        "What are you looking for...?", null, createSampleData(),
-                        new SearchResultListener<SampleModel>() {
+                        "What are you looking for...?", null, new ArrayList(),
+                        new SearchResultListener<Searchable>() {
                             @Override
                             public void onSelected(BaseSearchDialogCompat dialog,
-                                                   SampleModel item, int position) {
+                                                   Searchable item, int position) {
                                 Toast.makeText(MainActivity.this, item.getTitle(),
                                         Toast.LENGTH_SHORT).show();
                                 dialog.dismiss();
                             }
-                        }).show();
-            }
-        });
-        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+                        });
+        BaseFilter apiFilter = new BaseFilter() {
             @Override
-            public void onClick(View view) {
-                new ContactSearchDialogCompat<>(MainActivity.this, "Search...",
-                        "What are you looking for...?", null, createSampleContacts(),
-                        new SearchResultListener<ContactModel>() {
-                            @Override
-                            public void onSelected(BaseSearchDialogCompat dialog,
-                                                   ContactModel item, int position) {
-                                Toast.makeText(MainActivity.this, item.getTitle(),
-                                        Toast.LENGTH_SHORT).show();
-                                dialog.dismiss();
-                            }
-                        }).show();
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                doBeforeFiltering();
+                FilterResults results = new FilterResults();
+                results.values = new ArrayList<UserModel>();
+                results.count = 0;
+                try {
+                    ArrayList<UserModel> users = mUsersService
+                            .getFakeUsersBasedOnASearchTag(charSequence.toString())
+                            .execute()
+                            .body();
+                    if(users != null) {
+                        results.values = users;
+                        results.count = users.size();
+                    }
+                    return results;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
             }
-        });
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                if(filterResults != null) {
+                    ArrayList<UserModel> filtered = (ArrayList<UserModel>) filterResults.values;
+                    if(filtered != null)
+                        searchDialog.getFilterResultListener().onFilter(filtered);
+                    doAfterFiltering();
+                }
+            }
+        };
+        searchDialog.setFilter(apiFilter);
+        searchDialog.show();
+        searchDialog.setSearchHint("SSSSsss");
     }
 
     private ArrayList<SampleModel> createSampleData(){
---
+++ app/src/main/java/ir/mirrajabi/searchdialog/sample/customdialogs/services/UsersService.java
@@ -0,0 +1,13 @@
+package ir.mirrajabi.searchdialog.sample.customdialogs.services;
+
+import java.util.ArrayList;
+
+import ir.mirrajabi.searchdialog.sample.customdialogs.models.UserModel;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface UsersService {
+    @POST("api/users")
+    Call<ArrayList<UserModel>> getFakeUsersBasedOnASearchTag(@Body String tag);
+}
