54de38be5893c12df8a733525b844a90e4ad40ef
Mohammad Mirrajabi <mohammadmirrajabi@gmail.com>
2018-09-15 12:33:09 +0200

    Fixes #14
    Signed-off-by: Mohammad Mirrajabi <mohammadmirrajabi@gmail.com>

--- library/src/main/java/ir/mirrajabi/searchdialog/SimpleSearchFilter.java
+++ library/src/main/java/ir/mirrajabi/searchdialog/SimpleSearchFilter.java
@@ -1,8 +1,11 @@
 package ir.mirrajabi.searchdialog;
 
 import android.support.annotation.NonNull;
+import android.support.v4.util.Pair;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import ir.mirrajabi.searchdialog.core.BaseFilter;
@@ -19,6 +22,17 @@ public class SimpleSearchFilter<T extends Searchable> extends BaseFilter {
 	private ArrayList<T> mItems;
 	private FilterResultListener mFilterResultListener;
 	private boolean mCheckLCS;
+	private Comparator<Pair<T, Integer>> mComparator = new Comparator<Pair<T, Integer>>() {
+		@Override public int compare(Pair<T, Integer> o1, Pair<T, Integer> o2) {
+			if (o1 == null || o2 == null) {
+				return 0;
+			}
+			if (o1.second == null || o2.second == null) {
+				return 0;
+			}
+			return o2.second - o1.second;
+		}
+	};
 	
 	public SimpleSearchFilter(
 		List<T> objects, @NonNull FilterResultListener filterResultListener,
@@ -42,27 +56,33 @@ public class SimpleSearchFilter<T extends Searchable> extends BaseFilter {
 		doBeforeFiltering();
 		String filterSeq = chars.toString().toLowerCase();
 		FilterResults result = new FilterResults();
-		if (filterSeq != null && filterSeq.length() > 0) {
-			ArrayList<T> filter = new ArrayList<>();
-			for (T object : mItems) {
-				if (object.getTitle().toLowerCase().contains(filterSeq)) {
-					filter.add(object);
-				} else if (mCheckLCS) {
-					if (StringsHelper.lcs(object.getTitle(), filterSeq).length()
-					    > object.getTitle().length() * mAccuracyPercentage) {
-						filter.add(object);
-					}
+		if (filterSeq.length() <= 0) {
+			result.values = mItems;
+			result.count = mItems.size();
+			return result;
+		}
+		
+		ArrayList<Pair<T, Integer>> filteredItems = new ArrayList<>();
+		for (T item : mItems) {
+			if (item.getTitle().toLowerCase().contains(filterSeq)) {
+				filteredItems.add(new Pair<>(item, filterSeq.length()));
+			} else if (mCheckLCS) {
+				int lcsLength = StringsHelper.lcs(item.getTitle(), filterSeq).length();
+				if (lcsLength > item.getTitle().length() * mAccuracyPercentage) {
+					filteredItems.add(new Pair<>(item, lcsLength));
 				}
 			}
-			
-			result.values = filter;
-			result.count = filter.size();
-		} else {
-			synchronized (this) {
-				result.values = mItems;
-				result.count = mItems.size();
-			}
 		}
+		
+		Collections.sort(filteredItems, mComparator);
+		
+		ArrayList<T> finalResult = new ArrayList<>();
+		for (Pair<T, Integer> item : filteredItems) {
+			finalResult.add(item.first);
+		}
+		
+		result.values = finalResult;
+		result.count = finalResult.size();
 		return result;
 	}
 	
